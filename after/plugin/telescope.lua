f691c7f33042050758295aad0451309c4e0f6e84
syndrowm <syndrowm@gmail.com>
2023-01-21 10:25:36 -0700

    ctrl-p looks for git or does find_files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,12 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', function()
+    if pcall(builtin.git_files) then
+        builtin.git_files()
+    else
+        builtin.find_files()
+    end
+end)
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
