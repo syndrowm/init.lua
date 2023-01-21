7e1a5a47775cdfd4a9cafef8fac98e402efe1beb
syndrowm <syndrowm@gmail.com>
2023-01-21 10:24:38 -0700

    fix skeletons

--- lua/syn/skeleton.lua
+++ lua/syn/skeleton.lua
@@ -3,16 +3,13 @@
 -- autocmd BufNewFIle *.sh 0r ~/.vim/skeleton/skeleton.sh | norm G
 vim.api.nvim_create_autocmd("BufNewFile", {
   pattern = "*.py",
-  group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
   command = "0r ~/.config/nvim/skeleton/skeleton.py"
 })
 vim.api.nvim_create_autocmd("BufNewFile", {
   pattern = "*.c",
-  group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
   command = "0r ~/.config/nvim/skeleton/skeleton.c"
 })
 vim.api.nvim_create_autocmd("BufNewFile", {
   pattern = "*.sh",
-  group = vim.api.nvim_create_augroup("create_skeletons", { clear = true }),
   command = "0r ~/.config/nvim/skeleton/skeleton.sh"
 })
